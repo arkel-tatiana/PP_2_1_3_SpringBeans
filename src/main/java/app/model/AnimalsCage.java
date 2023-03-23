05e6ba161141f2b083e9377c610b6b7681520147
Arkel Tatiana <tata300-1@yandex.ru>
2023-03-23 15:49:49 +0700

    Задача 2_1_3

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,30 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+
+    public  AnimalsCage ( @Qualifier("dog") Animal animal , Timer timer ) {
+        this.animal = animal;
+        this.timer = timer ;
+    }
+    private Animal animal;
+    private Timer timer;
+
+     public Timer getTimer() {
+         return timer;
+     }
+     public void whatAnimalSay() {
+
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+}
